6225819e9cf6caf43d8e6b54f02994528263b388
Masahiro Kasashima <msr0210@gmail.com>
2019-12-06 22:54:57 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,103 @@
+Puppet Translator
+===============================================================
+
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+A Google Translate Command Line Interface
+
+
+Preface
+-------
+
+Translate passed text via Google Translate.
+
+This tool internally uses Google Chrome, so it should have been installed on
+your system. And you should also specify the path to the chrome. It can be
+done via a command line option or a environment variable
+`PUPPET_TRANSLATOR_CHROME_BIN_PATH`.
+
+It is supposed that this tool will work on every environment as long as you
+can browse https://translate.google.com by Google Chrome.
+
+
+Requirements
+------------
+
+- Node.js v7.6.0+
+
+    - This script uses async/await.
+
+Install
+-------
+
+    $ npm install -g puppet-translator
+
+Usage
+-----
+
+    $ puppet-translator [options]
+
+You must specify `-x` or `--text` option which represents the text to be
+translated.
+
+To know more about options, see below.
+
+### Options
+
+* -c, --chrome string
+
+    Path to the Google Chrome, if you omit this option a environment variable
+    `PUPPET_TRANSLATOR_CHROME_BIN_PATH` will be used instead.
+
+* -f, --from string
+
+    Language code of the input text or "auto" to autodetection  
+    (Default: "auto")
+
+* -t, --to string
+
+    Language code of the output text
+    (Default: "ja")
+
+* -x, --text string
+
+    Target text to translate
+
+* -h, --help
+
+    Show help
+
+### Examples
+
+* English to Japanese
+
+        $ puppet-translator -f en -t ja -x Hello
+        こんにちは
+
+* Japanese to English
+
+        $ puppet-translator -f ja -t en -x こんにちは
+        Hello
+
+`PUPPET_TRANSLATOR_CHROME_BIN_PATH` environment variable must be defined to
+point a Google Chrome binary before following command.
+
+Otherwise you should specify it in command line via `-c` / `--chrome` option.
+
+LICENSE
+-------
+
+[MIT License](LICENSE)
+
+AUTHOR
+------
+
+msr1k <msr0210+npm@gmail.com>
+
+CHANGELOG
+---------
+
+### v1.0.0 (2019-12-06)
+
+- First release.
+
---
+++ puppet-translator.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+const cla = require('command-line-args');
+const clu = require('command-line-usage');
+const pupp = require('puppeteer-core');
+
+const optionDefinitions = [
+  {
+    name: 'chrome',
+    alias: 'c',
+    type: String,
+    description: 'Path to the Google Chrome, if you omit this option a environment variable PUPPET_TRANSLATOR_CHROME_BIN_PATH will be used instead.'
+  },
+  {
+    name: 'from',
+    alias: 'f',
+    type: String,
+    defaultValue: 'auto',
+    description: 'Language code of the input text or "auto" to autodetection (Default: "auto")'
+  },
+  {
+    name: 'to',
+    alias: 't',
+    type: String,
+    defaultValue: 'ja',
+    description: 'Language code of the output text (Default: "ja")'
+  },
+  {
+    name: 'text',
+    alias: 'x',
+    type: String,
+    description: 'Target text to translate'
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Show help'
+  }
+];
+
+const sections = [
+  {
+    header: 'Puppet Translator - A Google Translate Command Line Interface -',
+    content: `Translate passed text via Google Translate.
+
+This tool internally uses Google Chrome, so it should have been installed on your system. \
+And you should also specify the path to the chrome. \
+It can be done via a command line option or a environment variable PUPPET_TRANSLATOR_CHROME_BIN_PATH.
+
+It is supposed that this tool will work on every environment \
+as long as you can browse https://translate.google.com by Google Chrome.
+`
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions
+  }
+];
+
+function onExit(errormsg) {
+  errormsg && console.log(errormsg);
+  const usage = clu(sections);
+  console.log(usage);
+  process.exit(errormsg ? 1 : 0);
+}
+
+var options;
+
+try {
+  options = cla(optionDefinitions);
+} catch {
+  onExit('ERROR: Could not parase command line arguments correctly.');
+}
+
+let chromePath = options.chrome || process.env.PUPPET_TRANSLATOR_CHROME_BIN_PATH;
+
+options.help && onExit();
+options.text || onExit('ERROR: `--text` was not given.');
+chromePath   || onExit("ERROR: Couldn't identify Google Chrome path");
+
+const url = `https://translate.google.com/?op=translate&sl=${options.from}&tl=${options.to}&text=${options.text}`;
+
+(async () => {
+
+  try {
+    const browser = await pupp.launch({
+      ignoreDefaultArgs: ['--disable-extensions'],
+      executablePath: chromePath
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const selector = ".tlid-translation.translation";
+    await page.waitForSelector(selector, {visible: true});
+    const data = await page.evaluate((s) => {
+      return document.querySelector(s).textContent;
+    }, selector);
+
+    console.log(data);
+    browser.close();
+  } catch {
+    console.error("Something went wrong.");
+  }
+})();
